8e4e4d46a2a3c48142512bd1d3086a1fced6b996
LudwigBoess <lboess@usm.lmu.de>
2021-08-17 15:33:08 +0200

    added docs

--- src/spectra/energy_spectrum.jl
+++ src/spectra/energy_spectrum.jl
@@ -1,8 +1,16 @@
-
-function getEnergySpectrum( CR_N::Vector{T}, CR_S::Vector{T}, 
+"""
+    getEnergySpectrum( CR_N::Vector{T}, CR_S::Vector{T}, 
                             CR_Cut::T, ρ::T; 
                             par::CRMomentumDistributionConfig, 
                             mode::Integer=3) where T
+    
+Compute the energy spectrum in ´[10^10 erg/g]´.
+"""
+function getEnergySpectrum( CR_N::Vector{T}, CR_S::Vector{T}, 
+                            CR_Cut::T, ρ::T;
+                            par::CRMomentumDistributionConfig, 
+                            mode::Integer=3) where T
+
 
     norm = Vector{Float64}(undef, par.Nbins)
     
@@ -20,7 +28,7 @@ function getEnergySpectrum( CR_N::Vector{T}, CR_S::Vector{T},
     bounds      = 10.0.^collect(log10(par.pmin):par.bin_width:log10(par.pmax))
     bound_up    = Vector{Float64}(undef, par.Nbins)
 
-    for i = 1:Nbins-1
+    for i = 1:par.Nbins-1
         bound_up[i] = (bounds[i+1] > CR_Cut) ? CR_Cut : bounds[i+1]
     end
 
@@ -30,16 +38,21 @@ function getEnergySpectrum( CR_N::Vector{T}, CR_S::Vector{T},
     energy      = Vector{Float64}(undef, par.Nbins)
 
     @inbounds for i = 1:par.Nbins
-        bin_centers[i] = 10.0^(0.5* (log10(bounds[i]) + log10(bound_up[i])))
+        bin_centers[i] = bounds[i] # 10.0^(0.5* (log10(bounds[i]) + log10(bound_up[i])))
         energy[i]      = energy_integral(bounds[i], bound_up[i], norm[i], CR_S[i], ρ)
+                         
     end
 
     return CR_EnergySpectrum(bin_centers, energy)
 end
 
 
-
-function getEnergySpectrum( spec::CRMomentumDistribution, ρ::T) where T
+"""
+    getEnergySpectrum( spec::CRMomentumDistribution, ρ::T ) where T
+    
+Compute the energy spectrum in ´[10^10 erg/g]´.
+"""
+function getEnergySpectrum( spec::CRMomentumDistribution, ρ::T ) where T
 
     Nbins = Int64( 0.5* size(spec.norm,1) )
     bin_centers = Vector{Float64}(undef, Nbins)
@@ -47,7 +60,7 @@ function getEnergySpectrum( spec::CRMomentumDistribution, ρ::T) where T
 
     k = 1
     @inbounds for i = 1:Nbins
-        bin_centers[i] = 10.0^(0.5* (log10(spec.bound[k]) + log10(spec.bound[k+1])))
+        bin_centers[i] = spec.bound[k] #10.0^(0.5* (log10(spec.bound[k]) + log10(spec.bound[k+1])))
         slope          = log10( spec.bound[k] / spec.bound[k+1] ) /
                          log10( spec.norm[k]  / spec.norm[k+1] )
         energy[i]      = energy_integral(spec.bound[k], spec.bound[k+1], spec.norm[k], slope, ρ)
