eecda1a9375d5800239f32f4e7b232b3daf5d2d6
Ludwig Böss <ludwig.boess@outlook.de>
2023-02-17 14:25:26 +0100

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,13 +55,13 @@ const ref_cr_B    = 5.e-6
             @testset "helper functions" begin
                 q = 4.2
                 α = SpectralCRsUtility.α_γ(q)
-                δ = δ_γ(α)
+                δ = SpectralCRsUtility.δ_γ(α)
 
                 @test α ≈ 2.2666666666666666
 
                 @test δ ≈ 0.4778013911548612
 
-                @test SpectralCRsUtility.σ_pp(α) ≈ 4.139054982268097e-23
+                @test SpectralCRsUtility.σ_pp(α) ≈ 4.2030549822680975e-26
 
                 @test SpectralCRsUtility.E_γ_powδ(50.0, δ) ≈ 23.505312382037395
 
@@ -69,131 +69,237 @@ const ref_cr_B    = 5.e-6
             end
 
             @testset "emissivity" begin 
-
-                pmin = 1.0
-                pmax = 1.e6
-                Nbins = length(ref_cr_norm)
-                bin_width = log10(pmax/pmin)/Nbins
-                bounds = [pmin * 10.0^((i - 1) * bin_width) for i = 1:Nbins+1]
-
-                @test γ_emission(ref_cr_norm, ref_cr_slope, ref_cr_cut, 
-                                1.0, 1.0, bounds) ≈ 564138.2640155746
+                @test gamma_source_PE04(1.0, 1.0, 4.5, 1.0) ≈ 3.3709004342715927e-17
             end
         end
 
         @testset "Werhahn et. al. (2021)" begin
             
-            @testset "Kafexhio et. al. (2014)" begin
+            @testset "Kafexhiu et. al. (2014)" begin
+
+                @testset "energy" begin
+                    # kinetic energy of proton in GeV
+                    @test SpectralCRsUtility.T_p(1.0) ≈ 0.938272088
+                    # Ratio between proton kinetic energy and rest energy
+                    @test SpectralCRsUtility.θ_p(1.0) ≈ 1.0657889249711967
+                    # Squared center of mass energy
+                    @test SpectralCRsUtility.𝓈(1.0) ≈ 5.397962220479519
+                    # Total energy of the pion in the centre-of-mass system
+                    @test SpectralCRsUtility.E_π_CM(1.0) ≈ 0.4077650184727047
+                    # Minimum allowed energy for the created pion given in the lab frame
+                    @test SpectralCRsUtility.E_π_min(1.0) ≈ 1.0045546908834024
+                    # Maximum allowed energy for the created pion given in the lab frame
+                    @test SpectralCRsUtility.E_π_max_LAB(2.0) ≈ 1.7130401721061672
+                    # Velocity in the center of mass system
+                    @test SpectralCRsUtility.β_CM(2.0) ≈ 0.7182781065151783
+                    # Pion maximum velocity in the lab frame
+                    @test SpectralCRsUtility.β_π_LAB(2.0) ≈ 0.996890937462484
+                    # Lorentz factor in the center of mass system 
+                    @test SpectralCRsUtility.γ_CM(2.0) ≈ 1.437285262211784
+                    # Lorentz factor of the pion in the lab system
+                    @test SpectralCRsUtility.γ_π_LAB(2.0) ≈ 12.691358092430386
+                    # Minimum γ-ray energy from a pion with kinetic energy `Tp`
+                    @test SpectralCRsUtility.E_γ_min(2.0) ≈ 0.0026629745121775988
+                    # Maximum γ-ray energy from a pion with kinetic energy `Tp`
+                    @test SpectralCRsUtility.E_γ_max(2.0) ≈ 1.7103771975939897
+                    # cross-referenced with `naima`
+                    @test SpectralCRsUtility.E_π_max_LAB(0.9) ≈ 0.6963603064421705
+                    @test SpectralCRsUtility.E_π_max_LAB(2.0) ≈ 1.7130401721061672
+                    @test SpectralCRsUtility.E_π_max_LAB(10.0) ≈ 9.586742675463034
+                    @test SpectralCRsUtility.E_π_max_LAB(50.0) ≈ 49.543400198671876
+                    @test SpectralCRsUtility.E_π_max_LAB(200.0) ≈ 199.5340716232349
+                end
 
                 @testset "cross-sections" begin
                     @testset "σ1π" begin
                         ref_values = [4.939730593781892e-44, 3.349616183683675e-28, 2.9277443259038443e-27, 4.205182345575466e-27, 4.2098467404072576e-27, 4.102902264540402e-27, 4.04135377875269e-27, 4.01908271987257e-27, 4.019322212939915e-27, 4.0305848981430185e-27]
-                        Tp = LinRange(Tp_th, 2.0, 10)
+                        Tp = LinRange(SpectralCRsUtility.Tp_th, 2.0, 10)
                         for i = 1:length(Tp)
-                            @test σ1π(Tp[i]) ≈ ref_values[i]
+                            @test SpectralCRsUtility.σ1π(Tp[i]) ≈ ref_values[i]
                         end
                     end
                     @testset "σ2π" begin
                         ref_values = Real[0, 0, 5.950065907462581e-30, 3.502162476195644e-29, 2.0111756307349467e-28, 1.0139566623278537e-27, 3.200131373967198e-27, 5.035149203274868e-27, 5.5755592120165185e-27, 5.678577195476546e-27]
-                        Tp = LinRange(Tp_th, 2.0, 10)
+                        Tp = LinRange(SpectralCRsUtility.Tp_th, 2.0, 10)
                         for i = 1:length(Tp)
-                            @test σ2π(Tp[i]) ≈ ref_values[i]
+                            @test SpectralCRsUtility.σ2π(Tp[i]) ≈ ref_values[i]
                         end
                     end
+                end
+
+                @testset "Fit parameters" begin
+                    @testset "α" begin
+                        @test SpectralCRsUtility.α_K14(10)  == 1
+                        @test SpectralCRsUtility.α_K14(100) == 1/2
+                    end
+                    @testset "β" begin
+                        @test SpectralCRsUtility.β_K14(0.9)    ≈ 3.0771079583046586
+                        @test SpectralCRsUtility.β_K14(1.5)    ≈ 2.7423614563386574
+                        @test SpectralCRsUtility.β_K14(5.5)    ≈ 4.983149979752324
+                        @test SpectralCRsUtility.β_K14(30.5)  == 4.2
+                        @test SpectralCRsUtility.β_K14(300.5) == 4.9
+                    end
+                    @testset "γ" begin
+                        @test SpectralCRsUtility.γ_K14(0.5)  == 0
+                        @test SpectralCRsUtility.γ_K14(3.5)   ≈ 1.6022183463205815
+                        @test SpectralCRsUtility.γ_K14(5.5)   ≈ 1.5220999865015488
+                        @test SpectralCRsUtility.γ_K14(25.5) == 1
+                    end
+                    @testset "κ" begin
+                        @test SpectralCRsUtility.κ_K14(10.0) ≈ 3.284251914874226
+                    end
+                    @testset "μ" begin
+                        @test SpectralCRsUtility.μ_K14(10.0) ≈ 0.00013093533160607907
+                    end
+                    @testset "𝒞" begin
+                        @test SpectralCRsUtility.𝒞(2.5) ≈ 0.1848674272287909
+                    end   
+                    @testset "𝒴_γ" begin
+                        @test SpectralCRsUtility.𝒴_γ(2.5) ≈ 2.501821876353361
+                    end
+                    @testset "𝒳_γ" begin
+                        @test SpectralCRsUtility.𝒳_γ(10.0, 2.5) ≈ 0.25041299505090747
+                    end
+                end
+
+                @testset "Amax" begin
+                    @test SpectralCRsUtility.A_max(0.2) == 0
+                    @test SpectralCRsUtility.A_max(0.9) ≈ 3.6445288415702625e-26
+                    @test SpectralCRsUtility.A_max(1.9) ≈ 6.990647621596554e-26
+                    @test SpectralCRsUtility.A_max(5.9) ≈ 1.073022582384389e-25
+                end
+
+                @testset "F" begin
+                    @test SpectralCRsUtility.F_K14(0.9, 1.0) == 0
+                    @test SpectralCRsUtility.F_K14(0.9, 2.0) == 0
+                    @test SpectralCRsUtility.F_K14(2.9, 1.0) ≈ 0.04135878590357172
+                    @test SpectralCRsUtility.F_K14(2.9, 2.0) ≈ 0.0010219312112906762
+                    @test SpectralCRsUtility.F_K14(5.9, 1.0) ≈ 0.07149128657886922
+                    @test SpectralCRsUtility.F_K14(5.9, 2.0) ≈ 0.008555466110591579
+                end
+
+                @testset "Differential Cross-Section" begin
+                    @test SpectralCRsUtility.dσγ_dEγ_K14(2.9, 1.0) ≈ 3.387987951017287e-27
+                    @test SpectralCRsUtility.dσγ_dEγ_K14(2.9, 2.0) ≈ 8.37135461058665e-29
                 end
 
                 @testset "𝓃_π0_K14" begin
                     ref_values = [0.1623951416932937, 0.5441722817877187, 1.1233635727421425, 1.9124386045483552, 2.946840094326904, 4.294155003380654, 5.99847652766958, 8.136370093740226, 10.877581752758365, 14.508457777229449]
                     Tp = 10.0 .^ LinRange(0, 5, 10)
                     for i = 1:length(Tp)
-                        @test 𝓃_π0_K14(Tp[i]) ≈ ref_values[i]
+                        @test SpectralCRsUtility.𝓃_π0_K14(Tp[i]) ≈ ref_values[i]
                     end
                 end
             end
 
-            @testset "Yang et. al. (2018)" begin
+            # @testset "Yang et. al. (2018)" begin
 
-                @testet "𝒮" begin
-                    @test 𝒮(0.5) ≈ 0.6224593312018546
-                end
+            #     @testset "𝒮" begin
+            #         @test 𝒮(0.5) ≈ 0.6224593312018546
+            #     end
 
-                @testset "x0" begin
-                    @test x0_Y18(1.0) == 0.17
-                    @test x0_Y18(3.0) == 0.1
-                    @test x0_Y18(6.0) == 0.08
-                end
+            #     @testset "x0" begin
+            #         @test x0_Y18(1.0) == 0.17
+            #         @test x0_Y18(3.0) == 0.1
+            #         @test x0_Y18(6.0) == 0.08
+            #     end
 
-                @testset "α" begin
-                    @test α_Y18(0.5) ≈ 0.3667105537514401
-                    @test α_Y18(1.2) == 0.36
-                    @test α_Y18(7.2) ≈ 0.5624265542500306
-                end
+            #     @testset "α" begin
+            #         @test α_Y18(0.5) ≈ 0.3667105537514401
+            #         @test α_Y18(1.2) == 0.36
+            #         @test α_Y18(7.2) ≈ 0.5624265542500306
+            #     end
 
-                @testset "β" begin
-                    @test β_Y18(0.5) ≈ 10.561742124018977
-                    @test β_Y18(1.2) == 12.3
-                    @test β_Y18(2.5) ≈ 18.500703568578157
-                    @test β_Y18(7.2) ≈ 28.5
-                end
+            #     @testset "β" begin
+            #         @test β_Y18(0.5) ≈ 10.561742124018977
+            #         @test β_Y18(1.2) == 12.3
+            #         @test β_Y18(2.5) ≈ 18.500703568578157
+            #         @test β_Y18(7.2) ≈ 28.5
+            #     end
 
-                @testset "γ" begin
-                    @test γ_Y18(0.5) ≈ 1.4342110750288033
-                    @test γ_Y18(1.2) == 0.68
-                    @test γ_Y18(2.5) ≈ 2.0014101708521315
-                end
+            #     @testset "γ" begin
+            #         @test γ_Y18(0.5) ≈ 1.4342110750288033
+            #         @test γ_Y18(1.2) == 0.68
+            #         @test γ_Y18(2.5) ≈ 2.0014101708521315
+            #     end
 
-                @testset "𝒩_low" begin
-                    @test 𝒩_low(0.5) ≈ 98.53507611819482
-                    @test 𝒩_low(1.2) ≈ 133.3177312831666
-                    @test 𝒩_low(2.5) ≈ 301.92303725497464
-                    @test 𝒩_low(7.2) ≈ 718.2181602894138
-                end
+            #     @testset "𝒩_low" begin
+            #         @test 𝒩_low(0.5) ≈ 98.53507611819482
+            #         @test 𝒩_low(1.2) ≈ 133.3177312831666
+            #         @test 𝒩_low(2.5) ≈ 301.92303725497464
+            #         @test 𝒩_low(7.2) ≈ 718.2181602894138
+            #     end
 
-                @testset "𝒩_high" begin
-                    @test 𝒩_high(0.5) ≈ 5.308245862802157
-                    @test 𝒩_high(1.2) ≈ 3.899779786674582
-                    @test 𝒩_high(2.5) ≈ 5.063538597169587
-                    @test 𝒩_high(7.2) ≈ 8.479678082265504
-                end
+            #     @testset "𝒩_high" begin
+            #         @test 𝒩_high(0.5) ≈ 5.308245862802157
+            #         @test 𝒩_high(1.2) ≈ 3.899779786674582
+            #         @test 𝒩_high(2.5) ≈ 5.063538597169587
+            #         @test 𝒩_high(7.2) ≈ 8.479678082265504
+            #     end
 
-                @testset "f_low" begin
-                    @test f_low(0.5, 0.5) ≈ 0.25067308096839785
-                    @test f_low(0.5, 1.2) ≈ 0.14221547465771092
-                    @test f_low(0.5, 2.5) ≈ 0.014504057755778775
-                    @test f_low(0.5, 7.2) ≈ 0.00023255732289278092
-                end
+            #     @testset "f_low" begin
+            #         @test f_low(0.5, 0.5) ≈ 0.25067308096839785
+            #         @test f_low(0.5, 1.2) ≈ 0.14221547465771092
+            #         @test f_low(0.5, 2.5) ≈ 0.014504057755778775
+            #         @test f_low(0.5, 7.2) ≈ 0.00023255732289278092
+            #     end
 
-                @testset "f_high" begin
-                    @test f_high(0.5, 0.5) ≈ 1.2956450984182628
-                    @test f_high(0.5, 1.2) ≈ 1.387873758732234
-                    @test f_high(0.5, 2.5) ≈ 0.930729399594792
-                    @test f_high(0.5, 7.2) ≈ 0.5288193022452127
-                end
+            #     @testset "f_high" begin
+            #         @test f_high(0.5, 0.5) ≈ 1.2956450984182628
+            #         @test f_high(0.5, 1.2) ≈ 1.387873758732234
+            #         @test f_high(0.5, 2.5) ≈ 0.930729399594792
+            #         @test f_high(0.5, 7.2) ≈ 0.5288193022452127
+            #     end
 
-                @testset "f_Y18" begin
-                    @test f_high(0.05, 0.5) ≈ 1.065464424851917
-                    @test f_high(0.05, 1.2) ≈ 1.2973863168276853
-                    @test f_high(0.05, 2.5) ≈ 1.9935155666320428
-                    @test f_high(0.05, 7.2) ≈ 4.857609138743655
+            #     @testset "f_Y18" begin
+            #         @test f_high(0.05, 0.5) ≈ 1.065464424851917
+            #         @test f_high(0.05, 1.2) ≈ 1.2973863168276853
+            #         @test f_high(0.05, 2.5) ≈ 1.9935155666320428
+            #         @test f_high(0.05, 7.2) ≈ 4.857609138743655
 
-                    @test f_high(0.5, 0.5) ≈ 0.8205183669119626
-                    @test f_high(0.5, 1.2) ≈ 0.8882392055886298
-                    @test f_high(0.5, 2.5) ≈ 0.6207630981667199
-                    @test f_high(0.5, 7.2) ≈ 0.23139728426253223
-                end
+            #         @test f_high(0.5, 0.5) ≈ 0.8205183669119626
+            #         @test f_high(0.5, 1.2) ≈ 0.8882392055886298
+            #         @test f_high(0.5, 2.5) ≈ 0.6207630981667199
+            #         @test f_high(0.5, 7.2) ≈ 0.23139728426253223
+            #     end
 
 
-            end
+            # end
 
             @testset "shared" begin
                 @testset "σ_pp_inel" begin
-                    @test σ_pp_inel(Tp_th) == 0.0
-                    @test σ_pp_inel(1.0)   ≈ 2.2519027368294765e-26
-                    @test σ_pp_inel(10.0)  ≈ 2.9469986501180466e-26
-                    @test σ_pp_inel(100.0) ≈ 3.1276509767139205e-26
-                    @test σ_pp_inel(1.e5)  ≈ 4.7856160923282935e-26
-                    @test σ_pp_inel(1.e6)  ≈ 5.719963756550566e-26
+                    @test SpectralCRsUtility.σ_pp_inel(SpectralCRsUtility.Tp_th) == 0.0
+                    @test SpectralCRsUtility.σ_pp_inel(1.0)   ≈ 2.2519027368294765e-26
+                    @test SpectralCRsUtility.σ_pp_inel(10.0)  ≈ 2.9469986501180466e-26
+                    @test SpectralCRsUtility.σ_pp_inel(100.0) ≈ 3.1276509767139205e-26
+                    @test SpectralCRsUtility.σ_pp_inel(1.e5)  ≈ 4.7856160923282935e-26
+                    @test SpectralCRsUtility.σ_pp_inel(1.e6)  ≈ 5.719963756550566e-26
                 end
+            end
+
+            # CR proton spectrum
+            norm = [3.3150304033868485e6, 205189.2010208896, 209.70705545666286, 0.171493213402529, 0.00016835520082165043, 1.5618300111348838e-7, 1.4777506904172816e-10, 1.413813951130306e-13]
+            slope = [1.7010470628738403, 3.941281318664551, 4.142595291137695, 4.0110182762146, 4.051901340484619, 4.037489414215088, 4.035932540893555, 4.048586368560791]
+            cut = 336271.59375
+            bounds = [0.1, 0.5623413251903492, 3.1622776601683795, 17.78279410038923, 100.0, 562.3413251903492, 3162.2776601683795, 17782.79410038923, 100000.0]
+            nH = 1.25218452056217e-5
+            V = 7.086811782958359e69
+            d = 3.562040251012591e26
+
+            @testset "Source" begin
+                @test gamma_source_pions(norm, slope, cut, bounds, nH, 2.0) ≈ 5.305080626464246e-30
+            end
+
+            @testset "Emissivity" begin
+                @test gamma_emissivity_pions(norm, slope, cut, bounds, nH, 2.0) ≈ 1.0610161252928492e-29
+            end
+
+            @testset "Luminosity" begin
+                @test gamma_luminosity_pions(norm, slope, cut, bounds, nH, V, N_integration_steps=100) ≈ 1.0735075827785918e42
+            end
+
+            @testset "Flux" begin
+                @test gamma_flux_pions(norm, slope, cut, bounds, nH, V, d, N_integration_steps=100) ≈ 4.081048534739771e-13
             end
         end
     end
